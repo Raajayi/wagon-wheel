268cebb159e417a56fd23ef90bc79be091457920
Indya Mandira <mandiraindy@gmail.com>
2023-08-30 15:40:55 +0800

    made profile page, edit profile and upload a profile pic

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  
 end
