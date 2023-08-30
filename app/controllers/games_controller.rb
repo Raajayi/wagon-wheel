63e2dac29d126027917b4e4f6e19ca19a4db0ab0
Ademide Ajayi <ademide.r.ajayi@gmail.com>
2023-08-30 16:03:37 +0800

    moved functionality to model class method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,20 +1,20 @@
 class GamesController < ApplicationController
   def index
     @games = Game.all
-    @games_completed = Game.all
+    @games_completed = Game.completed_games(current_user)
     # Getting the user from the games, knowing which responses were chosen
-    @games_long = Game.first.questions.first.answers.first.responses.first.user
+    # @games_long = Game.first.questions.first.answers.first.responses.first.user
     # Identify the current user
-    @user = current_user
-    # Find out the responses associated with that user
-    responses = @user.responses.all
-    # Find out which games those responses come from
-    games_responses = responses.map do |response|
-      response.answer.question.game
-    end
-    #
-    # Pull together the records for those games
-    @games_completed = games_responses.uniq
+    # @user = current_user
+    # # Find out the responses associated with that user
+    # responses = @user.responses.all
+    # # Find out which games those responses come from
+    # games_responses = responses.map do |response|
+    #   response.answer.question.game
+    # end
+    # #
+    # # Pull together the records for those games
+    # @games_completed = games_responses.uniq
 
   end
 
--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,17 @@ class Game < ApplicationRecord
   def total_score_for(user)
     responses.where(user: user).sum(:score)
   end
+
+  def self.completed_games(user)
+    responses = user.responses.all
+    # Find out which games those responses come from
+    unless responses.nil?
+      games_responses = responses.map do |response|
+        response.answer.question.game
+      end
+    end
+
+    # Pull together the records for those games
+    return games_responses.uniq
+  end
 end
