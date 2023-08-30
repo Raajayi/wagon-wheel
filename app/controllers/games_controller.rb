f1069ef9607c856d1564a30b1efbd16b83376b9a
Ademide Ajayi <ademide.r.ajayi@gmail.com>
2023-08-30 17:34:14 +0800

    completed games functionality now actually done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,23 +4,7 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
     @games_started = Game.started_games(current_user)
-
-
-    # Get the user's responses
-    responses = current_user.responses.all
-    # Get a list of the questions for the responses
-    @questions_with_responses = []
-    responses.each do |response|
-      @questions_with_responses << response.answer.question
-    end
-
-    @games_completed = @games_started.select do |game|
-      @questions_from_started_game = []
-      game.questions.each do |question|
-        @questions_from_started_game << question
-      end
-      @questions_from_started_game.to_set.subset?(@questions_with_responses.to_set)
-    end
+    @games_completed = Game.completed_games(current_user)
   end
 
   def show
--- app/models/game.rb
+++ app/models/game.rb
@@ -9,14 +9,24 @@ class Game < ApplicationRecord
 
   def self.started_games(user)
     responses = user.responses.all
-    # Find out which games those responses come from
     unless responses.nil?
       games_responses = responses.map do |response|
         response.answer.question.game
       end
+      return games_responses.uniq
     end
+  end
 
-    # Pull together the records for those games
-    return games_responses.uniq
+  def self.completed_games(user)
+    responses = user.responses.all
+
+    unless responses.nil?
+      questions_with_responses = responses.map { |response| response.answer.question }
+
+      games_completed = started_games(user).select do |game|
+        game.questions.to_set.subset?(questions_with_responses.to_set)
+      end
+      return games_completed
+    end
   end
 end
