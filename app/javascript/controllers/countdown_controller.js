2a0e38ffccd6603a367cd37d292dd3229a21dde7
Ademide Ajayi <ademide.r.ajayi@gmail.com>
2023-09-01 16:26:34 +0800

    countdown displayed but it does nothing

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -8,9 +8,7 @@ export default class extends Controller {
     seconds: Number
   }
 
-  updateTimer() {
-    this.contentTarget.innerHTML += "1second";
-  }
+
 
 
   connect() {
@@ -18,16 +16,36 @@ export default class extends Controller {
     console.log("I am the submit button target", this.submitTarget);
     console.log("I am the content target", this.contentTarget);
 
-    this.secondsValue = 29
-    console.log(this.secondsValue);
-
-    setInterval(updateTimer(), 1000);
+    // this.secondsValue = 29
+    // console.log(this.secondsValue);
+    // this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
+    // setInterval(updateTimer(), 1000);
+    this.countdown()
   }
 
+
+
   countdown() {
-    if (this.secondsValue  <= 0) {
-      console.log("Countdown is over");
-    }
+    console.log("Countdown has been activated");
+
+    this.secondsValue = 30
+    this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
+
+    // setInterval(this.updateTimer, 1000);
+    setInterval(() => {
+      this.secondsValue -= 1;
+      // console.log(this.secondsValue);
+      this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
+    }, 1000);
+    // if (this.secondsValue  <= 0) {
+    //   console.log("Countdown is over");
+    // }
+  }
+
+  updateTimer() {
+    this.secondsValue -= 1;
+    // console.log(this.secondsValue);
+    this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
   }
 
 
