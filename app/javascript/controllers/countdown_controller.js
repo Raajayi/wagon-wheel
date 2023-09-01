8d221dcfecf19e1af82cd469dc40c8a272e16418
Ademide Ajayi <ademide.r.ajayi@gmail.com>
2023-09-01 16:43:21 +0800

    code cleaned up

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -8,58 +8,19 @@ export default class extends Controller {
     seconds: Number
   }
 
-
-
-
   connect() {
-    // console.log("Controller is live", this.element);
-    // console.log("I am the submit button target", this.submitTarget);
-    // console.log("I am the content target", this.contentTarget);
-
-    // this.secondsValue = 29
-    // console.log(this.secondsValue);
-    // this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
-    // setInterval(updateTimer(), 1000);
     this.countdown()
   }
 
-
-
   countdown() {
-    console.log("Countdown has been activated");
-
-    this.secondsValue = 30
+    // Set the timer value here
+    this.secondsValue = 4
     this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
 
-    // setInterval(this.updateTimer, 1000);
     setInterval(() => {
       this.secondsValue -= 1;
-      // console.log(this.secondsValue);
       this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
-
-      if (this.secondsValue <= 0) {
-        this.submitTarget.click();
-      }
+      if (this.secondsValue <= 0) { this.submitTarget.click(); }
     }, 1000);
-
-    // if (this.secondsValue <= 0) {
-    //   clearInterval()
-    // }
-    // setInterval(this.updateTimer, 1000);
-    // if (this.secondsValue  <= 0) {
-    //   console.log("Countdown is over");
-    // }
   }
-
-  // updateTimer() {
-  //   // this.secondsValue -= 1;
-  //   console.log(this.secondsValue);
-  //   // this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
-  // }
-
-
-
-
-  // clearInterval()
-
 }
