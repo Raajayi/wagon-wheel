1fc5a2b2e0fea67fb906d6b69768020d1cabf168
phillip peet <phillip.peet@gmail.com>
2023-09-04 10:38:34 +0800

    Updates including adding logo to menu and showing remaining games on dashboard

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,12 +5,26 @@ class GamesController < ApplicationController
     @games = Game.all
     @games_started = Game.started_games(current_user)
     @games_completed = Game.completed_games(current_user)
+
     @user_first_name = current_user.first_name
     @user_last_name = current_user.last_name
     @user_email = current_user.email
+
+    @games_remaining = Game.all - Game.started_games(current_user)
+
+    @users = User.leaderboard_ranked
+    @games_count = {}
+    @games_count[:all] = Game.all.size
+    @games_count[:started] = Game.started_games(current_user).size
+    @games_count[:completed] = Game.completed_games(current_user).size
+
+
+    @games_remaining_count = @games_count[:all] - @games_count[:started]
+
   end
 
   def show
+    @users = User
     @game = Game.find(params[:id])
   end
 
