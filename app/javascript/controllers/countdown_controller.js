6c85b990399eb29ed9fbd5daae94effc8b542e83
Ademide Ajayi <ademide.r.ajayi@gmail.com>
2023-09-04 16:48:15 +0800

    functionlity done, need to disable form

--- app/javascript/controllers/countdown_controller.js
+++ app/javascript/controllers/countdown_controller.js
@@ -10,20 +10,21 @@ export default class extends Controller {
   }
 
   connect() {
-    console.log("Connected");
-    console.log("Correct answer id", this.correctAnswerIdValue);
+    // console.log("Connected");
+    // console.log("Correct answer id", this.correctAnswerIdValue);
     this.countdown();
 
-    const correctInputId = `response_answer_id_${this.correctAnswerIdValue}`
-    console.log(correctInputId);
-    console.log(document.getElementById(correctInputId));
-    const correctAnswerElement = document.getElementById(correctInputId)
-    console.dir(correctAnswerElement);
-    console.log(correctAnswerElement.labels[0]);
-    const correctLabelElement = correctAnswerElement.labels[0]
-    correctLabelElement.innerHTML = "LGTM!!!"
-    correctLabelElement.classList.add('bg-success')
-    correctAnswerElement.value = "DGWREFWR"
+    // const correctInputId = `response_answer_id_${this.correctAnswerIdValue}`
+    // console.log(correctInputId);
+    // console.log(document.getElementById(correctInputId));
+    // const correctInputElement = document.getElementById(correctInputId)
+    // // console.dir(correctInputElement);
+    // // console.log(correctInputElement.labels[0]);
+    // const correctLabelElement = correctInputElement.labels[0]
+    // // correctLabelElement.innerHTML = "LGTM!!!"
+    // correctLabelElement.classList.add('bg-success')
+    // correctLabelElement.classList.add('correct')
+    // correctInputElement.value = "DGWREFWR"
 
   }
 
@@ -32,39 +33,34 @@ export default class extends Controller {
     this.secondsValue = 10
     this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
 
-    const preSubmissionInterval = setInterval(() => {
-      this.secondsValue -= 1;
-      this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
-      if (this.secondsValue <= 0) {
-        this.submitTarget.click();
-        clearInterval(preSubmissionInterval)
-      }
-    }, 1000);
-  }
-
-  onSubmit(event) {
-    event.preventDefault();
-    // clearInterval(preSubmissionInterval)
-    this.secondsValue = 5;
-    this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
-
-    const postSubmissionInterval = setInterval(() => {
+    const preSelectionInterval = setInterval(() => {
       this.secondsValue -= 1;
       this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
       if (this.secondsValue <= 0) {
         // this.submitTarget.click();
-        clearInterval(postSubmissionInterval)
+        clearInterval(preSelectionInterval)
+        this.onTimerEnd()
       }
     }, 1000);
+  }
 
+  onTimerEnd() {
+    const correctInputId = `response_answer_id_${this.correctAnswerIdValue}`
+    const correctInputElement = document.getElementById(correctInputId)
+    const correctLabelElement = correctInputElement.labels[0]
+    correctLabelElement.classList.add('bg-success')
+    correctLabelElement.classList.add('correct')
 
-    console.dir(event);
-    console.dir(this.formTarget);
+    this.secondsValue = 5;
+    this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
 
-    // Set the timer for correct answer here
-
-    // Select the answer that happens to be correct.
-    // Change it's class to highlight it.
-    // X seconds later, submit form
+    const postSelectionInterval = setInterval(() => {
+      this.secondsValue -= 1;
+      this.contentTarget.innerHTML = `Time remaining: ${this.secondsValue}`;
+      if (this.secondsValue <= 0) {
+        clearInterval(postSelectionInterval)
+        this.submitTarget.click();
+      }
+    }, 1000);
   }
 }
