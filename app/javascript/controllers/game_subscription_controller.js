25b9ad50b139d1e4d572eccfd944e8c863ddd3c8
phillip peet <phillip.peet@gmail.com>
2023-09-05 13:17:35 +0800

    Updates to live connection and formatting question count

--- app/javascript/controllers/game_subscription_controller.js
+++ app/javascript/controllers/game_subscription_controller.js
@@ -4,17 +4,25 @@ import { createConsumer } from "@rails/actioncable"
 // Connects to data-controller="game-subscription"
 export default class extends Controller {
   static values = { gameId: Number }
-  static targets = [ "avatar" ]
+  static targets = [ "avatar", "avatarContainer" ]
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
       { channel: "GameChannel", id: this.gameIdValue },
       { received: (data) => {
-          console.log(this.avatarTargets[0].dataset)
-          const currentAvatar = this.avatarTargets.find(avatar => parseInt(avatar.dataset.userId) === data.userId) 
-          console.log(currentAvatar)
-          console.log(data) 
-          currentAvatar.classList.add("bg-success")
+          console.log(this.avatarTargets[0].dataset);
+          //const currentAvatar = this.avatarTargets.find(avatar => parseInt(avatar.dataset.userId) === data.userId);
+          // console.log(currentAvatar);
+          console.log(data);
+          //currentAvatar.classList.add("bg-success");
+
+          //Enable Avatar
+          const currentContainer = this.avatarContainerTargets.find(container => parseInt(container.dataset.userId) === data.userId);
+          console.log(currentContainer);
+          currentContainer.classList.add("connected");
+
+          //Enable Current User
+          //const currentUserContainer = this.
         }
       }
     )
